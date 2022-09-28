58de6f4e42401cd4433408e98d38d89ffbfa8f4b
navodsand <sandaruwannavod223@gmail.com>
2022-09-28 16:46:45 +0530

    recorrected

---
+++ index.js
@@ -0,0 +1,37 @@
+
+var grade;
+var mark = prompt("INPUT MARK");
+
+if (0 > mark) {
+    grade = "Mark Is invalid";
+    alert(grade);
+  } 
+  else if (0 <= mark & mark < 35) {
+    grade = "Grade Is w";
+    alert(grade);
+  }
+  else if (35 <= mark & mark < 55) {
+    grade = "Grade Is S";
+    alert(grade);
+  }
+   else if (55 <= mark & mark < 65) {
+    grade = "Grade Is C";
+    alert(grade);
+  }
+  else if (65 <= mark & mark < 75) {
+    grade = "Grade Is B";
+    alert(grade);
+  }
+  else if (75 <= mark & mark <= 100) {
+    grade = "Grade Is A";
+    alert(grade);
+  }
+  else if (100 < mark) {
+    grade = "Mark Is Invalid";
+    alert(grade);
+  }
+else
+
+
+
+alert(grade);
